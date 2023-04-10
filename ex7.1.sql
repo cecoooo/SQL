386d180766bcc05a24ec0b01563d25c7f5451564
Ceco <c_vatovski@abv.bg>
2023-04-10 12:35:38 +0300

    mess

--- ex7.1.sql
+++ ex7.1.sql
@@ -13,3 +13,72 @@ end &&
 delimiter ;
 
 call ex1('Ivan Todorov Petkov');
+
+delimiter //
+create procedure ex2(spId int)
+begin 
+select sp.name as 'sport', s.name as 'students', c.name as 'coaches'
+from sports as sp join sportgroups as sg on sp.id = sport_id
+join coaches as c on c.id = sg.coach_id
+join student_sport as ss on ss.sportgroup_id = sg.id
+join students as s on s.id = ss.student_id
+where sp.id = spId;
+end //
+delimiter ;
+
+call ex2(2);
+
+
+delimiter &&
+create procedure ex3(nameOfStudent varchar(255), yearOfPayment year, out res double)
+begin
+select avg(t.paymentAmount) from taxespayments as t
+join students as s on s.id = t.student_id
+where s.name = nameOfStudent and t.year = yearOfPayment;
+end &&
+delimiter ;
+
+drop procedure ex3;
+
+set @res = 0;
+call ex3('Iliyan Ivanov', '2022', @res);
+select @res;
+
+
+select * from students;
+select * from taxespayments;
+
+use transaction_test;
+
+delimiter //
+create procedure ex4(idProvider int, idRecipient int, sumAmount double)
+begin
+
+select @check := amount from customer_accounts
+where id = idProvider;
+
+update customer_accounts
+set amount = case 
+when amount - sumAmount >= 0 then amount - sumAmount
+else amount
+end
+where id = idProvider;
+
+update customer_accounts
+set amount = case 
+when @check >= sumAmount then amount + sumAmount
+else amount
+end
+where id = idRecipient;
+select if(row_count() = 0, 'Some error occured. Possible not enough money.', 'Successsfull.') as Massage;
+
+end //
+delimiter ;
+
+drop procedure ex4;
+
+call ex4(1, 3, 500);
+
+select * from customers;
+select * from customer_accounts;
+
