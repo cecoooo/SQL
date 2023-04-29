628dafb0cd80bd56bec2205b4b6d6ca501181005
Ceco <c_vatovski@abv.bg>
2023-04-29 17:46:58 +0300

    mess

--- tutorial.2.sql
+++ tutorial.2.sql
@@ -2,8 +2,8 @@ use uefa;
 
 -- cursors
 
-delimiter $
 drop procedure if exists get_all_clubs;
+delimiter $$
 create procedure get_all_clubs(inout club_list varchar(4000))
 begin 
 	declare finished int;
@@ -19,7 +19,8 @@ begin
         set club_list = concat(club, ';', club_list);
 	end loop getClub;
     close currClub;
-end $
+end 
+$$
 delimiter ;
 
 set @list_c = "";
@@ -27,4 +28,47 @@ call get_all_clubs(@list_c);
 select @list_c;
 
 
+-- handlers
+
 select * from clubs;
+delimiter $$
+create procedure pr()
+begin
+	declare er int;
+	declare continue handler for sqlexception set er = 1;
+end $$
+delimiter ;
+
+/*
+	for handlers:
+    declare action handler for consdition_value statement;
+    
+    - use declare for new handler
+    - action:
+		it can be 
+		- continue (the execution of the enclosing code block ( BEGIN … END ) continues) or 
+        - exit (the execution of the enclosing code block, where the handler is declared, terminates)
+	- condition_value:
+		it can be
+		- A MySQL error code
+        - sqlstate value such as sqlwarning, notfound, sqlexception
+        - a named condition associated with either a MySQL error code or sqlstate value
+	- statement:
+		could be:
+        - a simple statement
+        - begin...end statement
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
