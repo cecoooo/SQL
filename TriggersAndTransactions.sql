438d127e2d72c19e55e1275686f2bfe219928b1f
Ceco <c_vatovski@abv.bg>
2023-10-11 20:51:33 +0300

    new zad

--- TriggersAndTransactions.sql
+++ TriggersAndTransactions.sql
@@ -48,8 +48,53 @@ SET Balance /= 1.1
 WHERE id = 1;
 
 -- 3
-CREATE PROCEDURE usp_DepositMoney @AccountId INT, MoneyAmount MONEY
+CREATE OR ALTER PROCEDURE usp_DepositMoney @AccountId INT, @MoneyAmount DECIMAL(12, 4)
 AS
 BEGIN 
+	BEGIN TRANSACTION 
+	UPDATE Accounts
+	SET Balance += @MoneyAmount
+	WHERE Id = @AccountId
 	
+	IF @@ROWCOUNT <> 1 OR @MoneyAmount < 0
+	BEGIN
+		ROLLBACK;
+	END
+	COMMIT
 END
+
+-- 4
+CREATE OR ALTER PROCEDURE usp_WithdrawMoney @AccountId INT, @MoneyAmount DECIMAL(12, 4)
+AS
+BEGIN 
+	BEGIN TRANSACTION 
+	UPDATE Accounts
+	SET Balance -= @MoneyAmount
+	WHERE Id = @AccountId
+	
+	IF @@ROWCOUNT <> 1 OR @MoneyAmount < 0
+	BEGIN
+		ROLLBACK;
+	END
+	COMMIT
+END
+
+-- 5
+CREATE OR ALTER PROCEDURE usp_TransferMoney @SenderId INT, @ReceiverId INT, @Amount DECIMAL(12, 4)
+AS
+BEGIN
+	BEGIN TRANSACTION
+
+	UPDATE Accounts
+	SET Balance += @Amount
+	WHERE Id = @ReceiverId
+
+	UPDATE Accounts
+	SET Balance -= @Amount
+	WHERE Id = @SenderId
+
+	COMMIT
+END
+
+--Test
+usp_TransferMoney @SenderId = 2, @ReceiverId = 1, @Amount = 10
